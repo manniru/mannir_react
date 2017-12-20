47f30f7f7c27695f8b341cac21c4d6ce4560de7d
MUHAMMAD MANNIR AHMAD <manniru@gmail.com>
2017-12-20 10:26:57 +0800

    Add Material-UI-Next RaisedButton

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
-import React from 'react';
+//import React from 'react';
+//export default ({ name }) => <h1>BismillahirRahamanir Rahim {name}!</h1>;
 
-export default ({ name }) => <h1>BismillahirRahamanir Rahim {name}!</h1>;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButtonExampleSimple from './material-ui-next/RaisedButtonExampleSimple'
+
+const style = {
+  margin: 12,
+};
+
+const App = () => (
+  <MuiThemeProvider>
+
+    <RaisedButtonExampleSimple/>
+
+
+  </MuiThemeProvider>
+);
+
+//ReactDOM.render(<App />, document.getElementById('app'));
+
+export default App;
